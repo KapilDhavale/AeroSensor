f860678b12251a8a7cbebdbe0a27db84687294bf
Kapil Dhavale <kapildhavale602@gmail.com>
2025-10-13 19:18:51 +0530

    Initial commits

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,15 +1,14 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXgBt6ZGAEPMdSY39V8swJ6esAOWPDYlQ",
-  authDomain: "iotprojectexp8.firebaseapp.com",
-  databaseURL: "https://iotprojectexp8-default-rtdb.firebaseio.com",
-  projectId: "iotprojectexp8",
-  storageBucket: "iotprojectexp8.firebasestorage.app",
-  messagingSenderId: "645990672071",
-  appId: "1:645990672071:web:0980f0a51bfe94826700e2",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
